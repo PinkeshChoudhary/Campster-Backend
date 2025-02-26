9e0eb8e232c1df43f83623ce886eb361684491d2
Pinkesh Choudhary <pinkeshdudi01@gmail.com>
2025-02-26 18:49:33 +0530

    origin array

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://your-project.vercel.app"],
+    origin: ["https://campster-six.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'FETCH', 'PATCH'],
     credentials: true,
   },
@@ -34,7 +34,7 @@ connectDB();
 
 // Middleware
 app.use(express.json({ limit: '50mb' }));
-app.use(cors({ origin: "https://your-project.vercel.app", methods: ["GET", "POST", "PATCH", "PUT", "DELETE"] }));
+app.use(cors({ origin: "https://campster-six.vercel.app", methods: ["GET", "POST", "PATCH", "PUT", "DELETE"] }));
 
 // Routes
 app.use("/api", userRoutes);
